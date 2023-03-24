ea59a2680b5185c51f4e1382ef15b0841bc2f15a
Iida Björn <iida.w.bjorn@gmail.com>
2023-03-24 14:28:49 +0200

    new routes initialized

--- routes.py
+++ routes.py
@@ -5,3 +5,23 @@ from flask import abort, render_template, redirect, request, session
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    pass
+
+@app.route("/login")
+def login():
+    pass
+
+@app.route("/logout")
+def logout():
+    pass
+
+@app.route("newmessage")
+def new_message():
+    pass
+
+@app.route("/categories")
+def categories():
+    pass
