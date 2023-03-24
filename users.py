253851b7c63aa6aad3872e50c03f0adb291c4c7e
Iida Björn <iida.w.bjorn@gmail.com>
2023-03-24 14:52:13 +0200

    initialize users.py

---
+++ users.py
@@ -0,0 +1,43 @@
+import os
+from db import db
+from flask import session, request, abort
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def register(username, password):
+    hash_value = generate_password_hash(password)
+
+    try:
+        sql = "INSERT INTO users (username,password) VALUES (:username,:password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
+
+
+def login(username, password):
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            session["username"] = username
+            session["csrf_token"] = os.urandom(16).hex()
+            return True
+        else:
+            return False
+        
+def check_csrf():
+    if session["csrf_token"] != request.form["csrf_token"]:
+        abort(403)
+
+
+def user_id():
+    return session.get("user_id",0)
+
+def logout():
+    del session["user_id"]
+    del session["user_name"]
