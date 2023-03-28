88ccc2232a5e4922c41e7b717356bd7308492834
Iida Björn <iida.w.bjorn@gmail.com>
2023-03-28 18:36:19 +0300

    added functions to categories

--- categories.py
+++ categories.py
@@ -5,3 +5,13 @@ from sqlalchemy.sql import text
 def get_categories():
     sql = text("SELECT id, category_name FROM categories")
     return db.session.execute(sql).fetchall()
+
+def get_category_name(category_id):
+    sql = text("SELECT category_name FROM categories WHERE id=:id")
+    result = db.session.execute(sql, {"id":category_id})
+    return result.fetchone()[0]
+
+def get_category_messages(category_id):
+    sql = text("SELECT id, title, content, category_id FROM messages WHERE category_id=:id")
+    result = db.session.execute(sql, {"id":category_id})
+    return result.fetchall()
