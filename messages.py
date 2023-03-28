a5563f82e91d5118277e2b290c9d8beeb4730ec7
Iida Björn <iida.w.bjorn@gmail.com>
2023-03-28 18:34:42 +0300

    created messages.py

---
+++ messages.py
@@ -0,0 +1,16 @@
+from db import db
+from flask import session
+from sqlalchemy.sql import text
+import users
+
+def add_message(title, content, user_id, category_id):
+    sql = text("""INSERT INTO messages (title, content, visible, category_id, user_id)
+            VALUES (:title, :content, TRUE, :category_id, user_id) RETURNING id""")
+    result = db.session.execute(sql, {"title":title, "content":content, "category_id":category_id, "user_id":user_id}).fetchone()[0]
+    db.session.commit()
+    return result
+
+def get_messages():
+    sql = text("SELECT M.content, U.username FROM messages M, users U WHERE M.user_id=U.id ORDER BY M.id")
+    result = db.session.execute(sql)
+    return result.fetchall()
