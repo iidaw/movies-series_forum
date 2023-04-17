e23670768d977975e73831fe4242d89cf26cd9ac
Iida Björn <iida.w.bjorn@gmail.com>
2023-04-17 19:17:07 +0300

    added text() to sql

--- comments.py
+++ comments.py
@@ -1,8 +1,9 @@
 from db import db
 from flask import session
+from sqlalchemy.sql import text
 
 def add_comment(content, user_id, message_id):
-    sql = """INSERT INTO comments (content, user_id, message_id)
-            VALUES (:content, :user_id, :message_id)"""
+    sql = text("""INSERT INTO comments (content, user_id, message_id)
+            VALUES (:content, :user_id, :message_id)""")
     db.session.execute(sql, {"content":content, "user_id":user_id, "message_id":message_id})
     db.session.commit()
